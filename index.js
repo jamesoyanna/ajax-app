67532de88d681b2e1bb73caa677b38b424e92e0b
eshoiza <engrjayt200@gmail.com>
2020-06-09 00:48:39 +0100

    updated app

--- index.js
+++ index.js
@@ -1,43 +1,28 @@
-const textButton = document.getElementById("number");
-const apiButton = document.getElementById("chuck");
-const outputDiv = document.getElementById("output");
+const form = document.forms["todo"];
+form.addEventListener("submit", addTask, false);
 
-const textURL = "http://numbersapi.com/random";
-const apiURL = "https://api.chucknorris.io/jokes/random";
-
-textButton.addEventListener('click', ()=>{
-  fetch(textURL)
-  .then(response=>{
-    outputDiv.innerHTML = "Waiting for response....";
-    if(response.ok){
-      return response;
-    }
-    throw Error(response.statusText);
-  })
-  .then(response=>response.text())
-  .then(text =>outputDiv.innerHTML = text)
-  .catch(error=>console.log('There was an error',error))
-},false)
-
-
-apiButton.addEventListener('click', ()=>{
-  fetch(apiURL)
-  .then(response=>{
-    outputDiv.innerHTML = "Waitning response...";
-    if(response.ok){
-      return response;
-    }
-    throw Error(response.statusText);
-  })
-  .then(response=>response.json())
-  .then(data=>outputDiv.innerHTML = data.value)
-  .catch(error=>console.log("There was an error:", error))
-}, false);
-
-
-
-
-
-
-
-
+//Declear addtask function
+function addTask(event) {
+  event.preventDefault();
+  const number = form.task.value;
+  const task = {
+    userId: 1,
+    title: form.task.value,
+    completed: false,
+  };
+  const data = JSON.stringify(task);
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  const headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  });
+  const request = new Request(url, {
+    method: "POST",
+    header: headers,
+    body: data,
+  });
+  fetch(request)
+    .then((response) => response.json())
+    .then((task) => console.log(`Task saved with an id of ${task.id}`))
+    .catch((error) => console.log("There was an error", error));
+}
